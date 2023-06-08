34132fb6a970a25e2d38b3868c5eb784f2d64b4f
Ernelene <ernelene@cognician.com>
2023-06-08 16:22:42 +0200

    details page loads

--- src/app/details.cljs
+++ src/app/details.cljs
@@ -7,7 +7,19 @@
     [app.fetch-pets :as fetch-pets :refer [fetch-pets]]))
 
 (defui details [_]
-(let [result (useQuery {:details :id} fetch-pets)
-      id (.-id (useParams))]
-  (js/console.log (useParams))
-  ($ :h2 id)))
+(let [pet-id (.-id (useParams))
+      result (useQuery (clj->js ["details" pet-id]) fetch-pets)]
+  (js/console.log "in details" result)
+;;(js/console.log "is it loading?" (.-isLoading result))
+;;(println "pets is " (js->clj (.-pets (.-data result)):keywordize-keys true))
+ (if (.-isLoading result)
+    ($ :div {:className "loading-pane"}
+       ($ :h2 {:className "loader"} "🌀"))
+    (let [pet (first (js->clj (.-pets (.-data result)):keywordize-keys true))]
+      (println  "this is PET " pet)
+      ($ :div {:className "details"}
+       ($ :div
+          ($ :h1 (:name pet))
+          ($ :h2 (str (:animal pet) " - " (:breed pet) " - " (:city pet) " - " (:state pet))
+          ($ :button (str "Adopt " (:name pet)))
+          ($ :p (:description pet)))))))))
--- src/app/fetch_pets.cljs
+++ src/app/fetch_pets.cljs
@@ -1,13 +1,14 @@
-(ns app.fetch-pets)
+(ns app.fetch-pets
+  (:require
+   [shadow.cljs.modern :refer [js-await]]))
 
-(defn fetch-pets [query]
-  #_(let [id     (aget query 1)
-        result (js/fetch (str "http://pets-v2.dev-apis.com/pets?id=" id))]
-    (-> result
-        (.then (fn [response]
-                 (.json response)))
-    (.then (fn [json]
-             (let [json-data (js->clj json :keywordize-keys true)]
-               (if (.ok )
-                 json-data
-                 (js/Error. (str "details/" id " fetch not ok")))))))))
+(defn fetch-pets [context]
+(js/console.log context)
+  (let [query (.-queryKey context)
+        id    (aget query 1)
+        _     (println "id" id)]
+    (js-await [fetch-result (js/fetch (str "http://pets-v2.dev-apis.com/pets?id=" id))]
+              (js/console.log "my result" fetch-result #_(js->clj fetch-result))
+                              (if (.-ok fetch-result)
+                                 (.json fetch-result)
+                                (js/Error. (str "details/" id " fetch not ok"))))))
