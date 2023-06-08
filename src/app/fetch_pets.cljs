ce19667bfb157cd02a5b2fd03474e5d40f19d1c1
Ernelene <ernelene@cognician.com>
2023-06-08 12:59:44 +0200

    fetch-pets

---
+++ src/app/fetch_pets.cljs
@@ -0,0 +1,13 @@
+(ns app.fetch-pets)
+
+(defn fetch-pet [query]
+  (let [id     (aget query 1)
+        result (js/fetch (str "http://pets-v2.dev-apis.com/pets?id=" id))]
+    (-> result
+        (.then (fn [response]
+                 (.json response)))
+    (.then (fn [json]
+             (let [json-data (js->clj json :keywordize-keys true)]
+               (if (.ok )
+                 json-data
+                 (js/Error. (str "details/" id " fetch not ok")))))))))
